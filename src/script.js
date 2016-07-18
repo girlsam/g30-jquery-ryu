180a5ed1bbce267b7d92c8cf08cff70f488afb4a
Sam Goldsmith <samggold@gmail.com>
2016-07-18 14:38:21 -0600

    progress

---
+++ src/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // append an img tag with our picture of Ryu
+    $('#ryu').append('<img src="http://i.imgur.com/90Mmdcm.png">');
+
+    // change the img src on hover to the animated gif of Ryu
+    // url: http://i.imgur.com/nTj3Fxx.gif
+    // when the user 'unhovers' change back to static Ryu
+    $('#ryu > img').hover(function() {
+      this.src = "http://i.imgur.com/nTj3Fxx.gif";
+    }, function() {
+      this.src = "http://i.imgur.com/90Mmdcm.png";
+    });
+
+    // when a user clicks, change Ryu's stance
+    // url: http://i.imgur.com/Rfj0a80.png
+    $('#ryu > img').mousedown(function() {
+      this.src="http://i.imgur.com/Rfj0a80.png";
+    });
+
+    // add the Hadouken!
+    // url: http://i.imgur.com/oTyQRvX.gif
+    $('#ryu').mousedown(function() {
+      $(this).append('<img class="demo-hadouken"  src="http://i.imgur.com/oTyQRvX.gif">');
+    });
+
+    // animate that Hadouken
+
+    // let Ryu relax
+    // url: http://i.imgur.com/90Mmdcm.png
+
+});
